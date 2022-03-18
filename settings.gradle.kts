1f985eb93f4a850fae348f8e7ab50639245c3e79
JabDoesThings <JabDoesThings@github.com>
2022-03-18 17:11:44 -0500

    Added back Sledgehammer:ExamplePlugin & converted to Gradle-Kotlin project.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,4 @@ include(":CraftHammer_API")  // Universal API for CraftHammer & SledgeHammer.
 include(":CraftNail")        // Private repository for CraftHammer. (Legal reasons)
 include(":CraftHammer")      // Public repository for CraftHammer.
 include(":SledgeHammer")     // Plugin API for CraftHammer.
+include(":SledgeHammer:ExamplePlugin")
